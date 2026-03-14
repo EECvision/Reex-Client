867af6d3a8e8913291ea9d6bd070eb7af07f7824
EECvision <eec.studies@gmail.com>
2026-03-14 01:31:00 +0100

    update api-client import path

--- src/scripts/generator-utils.ts
+++ src/scripts/generator-utils.ts
@@ -484,7 +484,7 @@ export const generateModuleTemplate = (
     }
 
     // 2. Imports
-    contentParts.push('import { apiClient } from "../config";');
+    contentParts.push('import { apiClient } from "../api-client";');
     if (responseTypeImports) contentParts.push(responseTypeImports);
 
     // 3. Type Definitions
